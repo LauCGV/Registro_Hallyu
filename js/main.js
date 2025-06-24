1513b1bf1c7f5689cf56caf7742e177e5df331b0
LauCGV <lcvaldeblanquez@uninorte.edu.co>
2025-06-24 02:11:55 -0500

    link between the database and the project

--- js/main.js
+++ js/main.js
@@ -16,42 +16,46 @@ function handleCell() {
     // document.getElementById('nombre').value = '';
 
     let result_val = validar(nombre, apellido, edad, identificacion, correo, celular);
-    console.log("el resultado es: "+result_val + " y su tipo es "+ typeof result_val);
+    console.log("el resultado es: " + result_val + " y su tipo es " + typeof result_val);
+    if (result_val == true) {
+        agregar(nombre, apellido, edad, identificacion, correo, celular);
+    }
+
 }
 
 function validar(name, lastname, age, identification, email, cellphone) {
 
-    if(name.trim().length <= 0){
+    if (name.trim().length <= 0) {
         document.getElementById('nombre').value = '';
         alert("Debe ingresar un nombre");
         return false;
     }
 
-    if(lastname.trim().length <= 0){
+    if (lastname.trim().length <= 0) {
         alert("Debe ingresar un apellido");
         document.getElementById('apellido').value = '';
         return false;
     }
 
-    if(isNaN(parseInt(identification))){
+    if (isNaN(parseInt(identification))) {
         alert("Debe ingresar una identificacion que contenga solo números (sin carácteres especiales)");
         document.getElementById('identificacion').value = '';
         return false;
     }
 
-    if(age == "select"){
+    if (age == "select") {
         alert("Debe seleccionar una edad");
         document.getElementById('edad').option = "select";
         return false;
     }
 
-    if(!email.includes('@') || email.trim().length <= 1){
+    if (!email.includes('@') || email.trim().length <= 1) {
         alert("Debe ingresar un correo que cuente con '@'");
         document.getElementById('correo').value = '';
         return false;
     }
 
-    if(isNaN(parseInt(cellphone))){
+    if (isNaN(parseInt(cellphone))) {
         alert("Debe ingresar un número de celular que contenga solo números (sin carácteres especiales)");
         document.getElementById('celular').value = '';
         return false;
@@ -65,4 +69,45 @@ function validar(name, lastname, age, identification, email, cellphone) {
     console.log('Celular:', cellphone);
 
     return true;
+}
+
+function agregar(name, lastname, age, identification, email, cellphone) {
+    database.ref('contadorDeUsuarios').once('value')
+        .then((snapshot) => {
+            let contador = snapshot.val();
+
+            if (contador === null) {
+                contador = 0; // Si no existe, empezamos en 0
+            }
+
+            const numeroRegistro = contador + 1;
+            database.ref('usuarios').push({
+                numeroRegistro: numeroRegistro,
+                nombre: name,
+                apellido: lastname,
+                edad: age,
+                identificacion: identification,
+                correo: email,
+                celular: cellphone
+            })
+                .then(() => {
+                    database.ref('contadorDeUsuarios').set(numeroRegistro);
+                })
+                .then(() => {
+                    alert('Registro exitoso! Su número de registro es: '+numeroRegistro);
+
+                    // Limpiar los campos del formulario después de registrar
+                    document.getElementById('nombre').value = '';
+                    document.getElementById('apellido').value = '';
+                    document.getElementById('edad').value = 'select';
+                    document.getElementById('identificacion').value = '';
+                    document.getElementById('correo').value = '';
+                    document.getElementById('celular').value = '';
+                })
+                .catch((error) => {
+                    alert.log("Se presentó un error en el sistema. Reinicie la página y vuelva a intentarlo. Disculpe las molestias", error);
+                });
+
+        }
+        )
 }
